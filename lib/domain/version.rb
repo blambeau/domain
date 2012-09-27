bf21662cf5811dd51074336eb39912ef11126f06
Bernard Lambeau <blambeau@gmail.com>
2012-09-27 18:07:49 +0200

    Bump version to 1.0.0.rc1

--- lib/domain/version.rb
+++ lib/domain/version.rb
@@ -1,9 +1,9 @@
 module Domain
   module Version
 
-    MAJOR = 0
-    MINOR = 1
-    TINY  = 0
+    MAJOR = 1
+    MINOR = 0
+    TINY  = "0.rc1"
 
     def self.to_s
       [ MAJOR, MINOR, TINY ].join('.')
