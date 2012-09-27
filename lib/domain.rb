f9123be85cbf928081231492117f9533858e28db
Bernard Lambeau <blambeau@gmail.com>
2012-09-27 11:43:11 +0200

    Add Domain.sbyc

--- lib/domain.rb
+++ lib/domain.rb
@@ -3,6 +3,21 @@
 #
 module Domain
 
+  # Creates a domain through specialization by constraint
+  #
+  # @param [Class] superdom
+  #   the superdomain of the created domain
+  # @param [Proc] pred
+  #   the domain predicate
+  #
+  # @return [Class]
+  #   the created domain as a ruby Class
+  #
+  # @api public
+  def self.sbyc(superdom = Object, subdoms = [], &pred)
+    Class.new(superdom){ include SByC.new(superdom, subdoms, pred) }
+  end
+
 end # module Domain
 require_relative 'domain/version'
 require_relative 'domain/loader'
